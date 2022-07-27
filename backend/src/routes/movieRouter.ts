df7fa20dd3dcfbd033ee528014cecaed0bd52d49
Gansukh Batjargal <gansukhb@Gansukhs-MacBook-Pro.local>
2022-07-27 20:27:34 +0800

    feat: precommit eslint

--- backend/src/routes/movieRouter.ts
+++ backend/src/routes/movieRouter.ts
@@ -1,24 +1,18 @@
-
-
-
-
-
+import express from 'express';
 import {
   getMoviesService,
   getMovieService,
   createMovieService,
   deleteMovieService,
   updateMovieService,
-} from "../controller/movieControllers";
-
-import express from "express";
+} from '../controller/movieControllers';
 
 const movieRouter = express.Router();
 
-movieRouter.get("/movies", getMoviesService);
-movieRouter.get("/movie/:id", getMovieService);
-movieRouter.patch("/movie/:id", updateMovieService);
-movieRouter.post("/movie", createMovieService);
-movieRouter.delete("/movie", deleteMovieService);
+movieRouter.get('/movies', getMoviesService);
+movieRouter.get('/movie/:id', getMovieService);
+movieRouter.patch('/movie/:id', updateMovieService);
+movieRouter.post('/movie', createMovieService);
+movieRouter.delete('/movie', deleteMovieService);
 
 export default movieRouter;
