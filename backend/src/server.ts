e03bd0dd6cc07e0050b4d3f3c8f3aaa3dacf9050
Narantsatsralt05 <n.bumnasan@gmail.com>
2022-07-29 18:22:05 +0800

    chore: changed backend run script also added dev script in root folder

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,7 +4,7 @@ import app from "./app";
 
 dotenv.config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const uri: string = process.env.MONGODB_URL || "mongodb://localhost:27017/nest_hr";
 
 mongoose.connect(uri);
