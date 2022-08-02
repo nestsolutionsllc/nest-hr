a74176bdd9a4122cde8462229822c8bb865f63df
Duluu <Duluu@MacBook-Pro-3.local>
2022-08-02 17:58:46 +0800

    feat: Personal info screen

---
+++ frontend/src/components/profile/mockData.ts
@@ -0,0 +1,23 @@
+export type InfoType = {
+  lastName: string;
+  firstName: string;
+  departmentName: string;
+  position: string;
+  email: string;
+  salary: string;
+  joiningDate: Date;
+  userPhoto: string;
+};
+export const INFO_MOCK_DATA: InfoType = {
+  lastName: "Bill",
+  firstName: "Duluu",
+  departmentName: "Technology",
+  position: "Software Developer",
+  email: "duluuf@gmail.com",
+  salary: "99999999",
+  joiningDate: new Date(),
+  userPhoto:
+    "https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes.png",
+};
+
+export const SALARY_MOCK_DATA = {};
