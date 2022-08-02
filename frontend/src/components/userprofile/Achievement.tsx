3c2110742799e727c02493e495542bdaafacc14a
geegii <gegiimz96@gmail.com>
2022-08-02 13:26:56 +0800

    feat: create sidebar in user profile & structure of other related sections

---
+++ frontend/src/components/userprofile/Achievement.tsx
@@ -0,0 +1,14 @@
+import { Typography, Stack } from "@mui/material";
+
+const Achievement = () => {
+  return (
+    <Stack>
+      <Typography variant="h4" sx={{ borderBottom: 1, paddingBottom: 2, borderColor: "#f0f2f5" }}>
+        Achievement
+      </Typography>
+      <Typography variant="body1">Achievemen section</Typography>
+    </Stack>
+  );
+};
+
+export default Achievement;
