88805e83061dc5b5879236e4c64de69e79be23a2
geegii <gegiimz96@gmail.com>
2022-08-03 15:46:25 +0800

    feat: rebase with develop

--- frontend/src/theme/theme.ts
+++ frontend/src/theme/theme.ts
@@ -1,15 +1,6 @@
 import { createTheme, Theme } from "@mui/material";
 
 export const theme: Theme = createTheme({
-  breakpoints: {
-    values: {
-      xs: 0,
-      sm: 600,
-      md: 900,
-      lg: 1200,
-      xl: 1536,
-    },
-  },
   palette: {
     primary: {
       main: "#0070c0",
