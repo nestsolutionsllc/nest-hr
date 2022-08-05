99c4cc103d52d3a268a06f3e0e789f34dae37681
Od-Erdene <69565961+oderdene07@users.noreply.github.com>
2022-08-05 14:51:22 +0800

    feat: implemented org chart (#105)
    good. next improvement: js to ts, css dry principle, create employee interface

---
+++ frontend/src/components/orgchart/ExpandButton.js
@@ -0,0 +1,39 @@
+import React from "react";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+
+const styles = {
+  expandBtn: {
+    width: "30px",
+    height: "30px",
+    margin: "auto",
+    color: "#227c9d",
+    backgroundColor: "#fef9ef",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    border: "2px solid #d3d3d3",
+    borderRadius: "50%",
+    cursor: "pointer",
+  },
+  icon: {
+    width: "50%",
+    display: "flex",
+    fontSize: "1rem",
+  },
+};
+
+const ExpandButton = node => {
+  return (
+    <>
+      {node && (
+        <div style={styles.expandBtn}>
+          <span>{node.data._directSubordinates}</span>
+          {node.children ? <KeyboardArrowUpIcon style={styles.icon} /> : <KeyboardArrowDownIcon style={styles.icon} />}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default ExpandButton;
