027f8da3f017aa24b8b19561866cf41bad14bfba
0hmoony <moonywlk@gmail.com>
2022-08-05 11:46:24 +0800

    Feature: Credit Card Component

--- frontend/src/components/userprofile/CreditCard.tsx
+++ frontend/src/components/userprofile/CreditCard.tsx
@@ -1,7 +1,6 @@
 import Cards from "react-credit-cards";
 import "react-credit-cards/es/styles-compiled.css";
 import { useState, useRef, useEffect } from "react";
-import SalaryPage from "../../pages/salaryInfo";
 
 export default function CardComp(props) {
   const [number, setNumber] = useState("4242424242424242");
@@ -14,79 +13,6 @@ export default function CardComp(props) {
   return (
     <div className="App">
       <Cards number={number} name={name} expiry={expiry} cvc={cvc} focused={focus} />
-      <div>
-        <form>
-          <input
-            style={{
-              backgroundColor: "#fff",
-              border: "1px solid #ced4da",
-              borderRadius: "8px",
-              color: "#222",
-              fontSize: "16px",
-              padding: "8px 16px",
-              margin: "10px auto",
-              display: "block",
-            }}
-            type="tel"
-            name="number"
-            placeholder="Card Number"
-            onChange={e => setNumber(e.target.value)}
-            onFocus={e => setFocus(e.target.name)}
-            ref={ref}
-          />
-          <input
-            style={{
-              backgroundColor: "#fff",
-              border: "1px solid #ced4da",
-              borderRadius: "8px",
-              color: "#222",
-              fontSize: "16px",
-              padding: "8px 16px",
-              margin: "10px auto",
-              display: "block",
-            }}
-            type="text"
-            name="name"
-            placeholder="Name"
-            onChange={e => setName(e.target.value)}
-            onFocus={e => setFocus(e.target.name)}
-          />
-          <input
-            style={{
-              backgroundColor: "#fff",
-              border: "1px solid #ced4da",
-              borderRadius: "8px",
-              color: "#222",
-              fontSize: "16px",
-              padding: "8px 16px",
-              margin: "10px auto",
-              display: "block",
-            }}
-            type="text"
-            name="expiry"
-            placeholder="MM/YY"
-            onChange={e => setExpiry(e.target.value)}
-            onFocus={e => setFocus(e.target.name)}
-          />
-          <input
-            style={{
-              backgroundColor: "#fff",
-              border: "1px solid #ced4da",
-              borderRadius: "8px",
-              color: "#222",
-              fontSize: "16px",
-              padding: "8px 16px",
-              margin: "10px auto",
-              display: "block",
-            }}
-            type="tel"
-            name="cvc"
-            placeholder="CVC"
-            onChange={e => setCvc(e.target.value)}
-            onFocus={e => setFocus(e.target.name)}
-          />
-        </form>
-      </div>
     </div>
   );
 }
