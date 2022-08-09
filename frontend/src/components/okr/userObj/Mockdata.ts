ad8f578fb99d633f131449b6cf51f870566feb28
sumiyab <sumiyabazaramarjargal@gmail.com>
2022-08-09 15:13:48 +0800

    refactor: refactored okr objectives

---
+++ frontend/src/components/okr/userObj/Mockdata.ts
@@ -0,0 +1,10 @@
+import { UserObjectiveType } from "./type";
+
+const OBJECTIVE_INFO_MOCK_DATA: UserObjectiveType = {
+  activeGoals: 18,
+  progress: 20,
+  completed: 5,
+  dueDate: 19,
+};
+
+export default OBJECTIVE_INFO_MOCK_DATA;
